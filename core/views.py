b16e06ef878be7f71c3c51daad36b221d8ffa2f7
mohamedmostafa33 <212103807@o6u.edu.eg>
2025-01-28 21:06:18 +0200

    final update

--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.views import LogoutView as BaseLogoutView
-from django.urls import reverse_lazy
+
 from item.models import Category, Item
+
 from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
+
     return render(request, 'core/index.html', {
         'categories': categories,
         'items': items,
@@ -21,12 +22,14 @@ def about(request):
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
+
         if form.is_valid():
             form.save()
-            return redirect('core:login')   
+
+            return redirect('/login/')
     else:
         form = SignupForm()
-    return render(request, 'core/signup.html', {'form': form})
 
-class LogoutView(BaseLogoutView):
-    next_page = reverse_lazy('core:login')   
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
--- puddle/settings.py
+++ puddle/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Application definition
@@ -124,10 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
